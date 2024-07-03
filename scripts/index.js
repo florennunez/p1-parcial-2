afb59ce4d789cc5c9a7079a3994427278f3ae25f
Flor <fflor2297@gmail.com>
2024-07-03 14:55:07 -0300

    catalogoCarrito

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    const catalogo = new Catalogo();
-    const carrito = new Carrito(carrito); // Se crea una instancia de la clase Carrito
+    const catalogo = new Carrito();
+    const carrito = new Catalogo(carrito); // Se crea una instancia de la clase Carrito
 
     fetch('productos.json')
         .then(response => response.json())
