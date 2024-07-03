02b4f6effaa79500ab720a8a6621bf6cff5e942d
Flor <fflor2297@gmail.com>
2024-07-03 09:34:58 -0300

    border

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -25,7 +25,7 @@ class Catalogo {
     productosFiltrados.forEach(producto => {
         // Crear contenedor para cada producto
         const productoDiv = document.createElement('div');
-        productoDiv.classList.add('producto');
+        productoDiv.classList.add('producto border-start border border-secondary rounded-1');
 
         // Crear y configurar elementos para imagen, nombre, descripción y precio
         const imagenEl = document.createElement('img');
