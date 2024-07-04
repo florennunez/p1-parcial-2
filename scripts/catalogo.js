b7ac1228de147c8d93066779e4e416c686e43051
Flor <fflor2297@gmail.com>
2024-07-03 23:26:55 -0300

    id carrito

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -19,42 +19,42 @@ class Catalogo {
         const catalogoDiv = document.getElementById('productos');
         // Limpiar el contenido actual
         while (catalogoDiv.firstChild) {
-        catalogoDiv.removeChild(catalogoDiv.firstChild);
-    }
-    const productosFiltrados = this.filtrarProductos(categoria);
+            catalogoDiv.removeChild(catalogoDiv.firstChild);
+        }
+        const productosFiltrados = this.filtrarProductos(categoria);
 
-    productosFiltrados.forEach(producto => {
-        // Crear contenedor para cada producto
-        const productoDiv = document.createElement('div');
-        productoDiv.classList.add('producto');
+        productosFiltrados.forEach(producto => {
+            // Crear contenedor para cada producto
+            const productoDiv = document.createElement('div');
+            productoDiv.classList.add('producto');
 
-        // Crear y configurar elementos para imagen, nombre, descripción y precio
-        const imagenEl = document.createElement('img');
-        imagenEl.src = producto.imagen;
-        imagenEl.alt = producto.categoria;
-        const nombreEl = document.createElement('h2');
-        nombreEl.textContent = producto.nombre;
-        const descripcionEl = document.createElement('p');
-        descripcionEl.textContent = producto.descripcion;
-        const precioEl = document.createElement('p');
-        precioEl.textContent = `Precio: $${producto.precio}`;
+            // Crear y configurar elementos para imagen, nombre, descripción y precio
+            const imagenEl = document.createElement('img');
+            imagenEl.src = producto.imagen;
+            imagenEl.alt = producto.nombre;
+            const nombreEl = document.createElement('h2');
+            nombreEl.textContent = producto.nombre;
+            const descripcionEl = document.createElement('p');
+            descripcionEl.textContent = producto.descripcion;
+            const precioEl = document.createElement('p');
+            precioEl.textContent = `Precio: $${producto.precio}`;
 
-        // Agregar boton del carrito
-        const agregarBoton = document.createElement('button');
-        agregarBoton.textContent = 'Agregar al carrito';
-        agregarBoton.addEventListener('click', () => {
-        this.carrito.agregarProducto(producto);
-        });
+            // Agregar boton del carrito
+            const agregarBoton = document.createElement('button');
+            agregarBoton.textContent = 'Agregar al carrito';
+            agregarBoton.addEventListener('click', () => {
+                this.carrito.agregarProducto(producto);
+            });
 
-        // Agregar elementos al div del producto
-        productoDiv.appendChild(imagenEl);
-        productoDiv.appendChild(nombreEl);
-        productoDiv.appendChild(descripcionEl);
-        productoDiv.appendChild(precioEl);
-        productoDiv.appendChild(agregarBoton);
+            // Agregar elementos al div del producto
+            productoDiv.appendChild(imagenEl);
+            productoDiv.appendChild(nombreEl);
+            productoDiv.appendChild(descripcionEl);
+            productoDiv.appendChild(precioEl);
+            productoDiv.appendChild(agregarBoton);
 
-        // Agregar el div del producto al contenedor del catálogo
-        catalogoDiv.appendChild(productoDiv);
+            // Agregar el div del producto al contenedor del catálogo
+            catalogoDiv.appendChild(productoDiv);
         });
     }
 }
