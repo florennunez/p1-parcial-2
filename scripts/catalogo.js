d762dc69ee633401a99d563c6b211cea13d64966
Flor <fflor2297@gmail.com>
2024-07-10 18:39:53 -0300

    modal

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -47,64 +47,6 @@ class Catalogo {
             modalDescripcion.setAttribute('data-toggle', 'modal');
             modalDescripcion.setAttribute('data-target', '#modalDescripcion');
 
-            modalDescripcion.addEventListener('click', () => {  
-                const modal = document.createElement('div');
-                modal.classList.add('modal');
-                modal.setAttribute('tabindex', '-1');
-                modal.setAttribute('role', 'dialog');
-
-                // Crear el diálogo del modal
-                const modalDialog = document.createElement('div');
-                modalDialog.classList.add('modal-dialog');
-                modalDialog.setAttribute('role', 'document');
-                modal.appendChild(modalDialog);
-
-                // Crear el contenido del modal
-                const modalContent = document.createElement('div');
-                modalContent.classList.add('modal-content');
-                modalDialog.appendChild(modalContent);
-
-                // Crear el encabezado del modal
-                const modalHeader = document.createElement('div');
-                modalHeader.classList.add('modal-header');
-                modalContent.appendChild(modalHeader);
-                
-                const modalTitle = document.createElement('h5');
-                modalTitle.classList.add('modal-title');
-                modalTitle.textContent = producto.nombre;
-                modalHeader.appendChild(modalTitle);
-
-                const closeButton = document.createElement('button');
-                closeButton.type = 'button';
-                closeButton.classList.add('btn-close');
-                closeButton.setAttribute('data-bs-dismiss', 'modal');
-                closeButton.setAttribute('aria-label', 'Close');
-                modalHeader.appendChild(closeButton);
-
-                    // Crear el cuerpo del modal
-                const modalBody = document.createElement('div');
-                modalBody.classList.add('modal-body');
-                const modalText = document.createElement('p');
-                modalText.textContent = producto.descripcion;
-                modalBody.appendChild(modalText);
-                modalContent.appendChild(modalBody);
-
-                // Crear el footer del modal
-                const modalFooter = document.createElement('div');
-                modalFooter.classList.add('modal-footer');
-                modalContent.appendChild(modalFooter);
-
-                const closeButtonFooter = document.createElement('button');
-                closeButtonFooter.type = 'button';
-                closeButtonFooter.classList.add('btn', 'btn-secondary');
-                closeButtonFooter.setAttribute('data-bs-dismiss', 'modal');
-                closeButtonFooter.textContent = 'Close';
-                modalFooter.appendChild(closeButtonFooter);
-
-                // Agregar el modal al DOM, por ejemplo, al final del body o cerca del elemento del producto
-                document.body.appendChild(modal);
-            });
-
             // Agregar boton del carrito
             const agregarBoton = document.createElement('button');
             agregarBoton.classList.add('btn');
@@ -112,7 +54,6 @@ class Catalogo {
             agregarBoton.textContent = 'Agregar al carrito';
             agregarBoton.addEventListener('click', () => {
                 this.carrito.agregarProducto(producto);
-
             });
 
             // Agregar elementos al div del producto
