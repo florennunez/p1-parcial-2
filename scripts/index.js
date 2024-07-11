e1e6759f916c6f35f4ffedba7816d2b27314656f
Flor <fflor2297@gmail.com>
2024-07-11 09:05:09 -0300

    ffuncion

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+let contadorCarrito = 0;
+
+function mostrarNotificacion() {
+    if (contadorCarrito === 0) {
+        const notificacion = document.getElementById('notificacion');
+        notificacion.style.display = 'none';
+    }
+    else {
+        const notificacion = document.getElementById('notificacion');
+        notificacion.style.display = 'block';
+        notificacion.innerHTML = contadorCarrito;
+    }
+};
